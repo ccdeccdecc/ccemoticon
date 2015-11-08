8d005ab8607ecf1a85bcd37cba9e0b01d5fc23ed
apple <apple@woshi39.local>
2015-11-08 15:20:08 +0800

    新建控制器CCEmoticonViewController

---
+++ 表情键盘/表情键盘/Emoticon/CCEmoticonViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CCEmoticonViewController.swift
+//  表情键盘
+//
+//  Created by apple on 15/11/8.
+//  Copyright © 2015年 eason. All rights reserved.
+//
+
+import UIKit
+
+class CCEmoticonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+
+}
